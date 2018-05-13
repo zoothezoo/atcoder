f5a08177707d3d9729664709f3b4b4236b7b6781
zoothezoo <kohekoshien@gmail.com>
2018-05-14 02:51:15 +0900

    new

--- twoCoins/twoCoins.kt
+++ twoCoins/twoCoins.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>){
     val coins = readLine()!!.split(" ")
-    val intCoins :IntArray = IntArray(3)
-    for(i in coins.indices){
-        intCoins[i] = coins[i].toInt()
-    }
+        val intCoins :IntArray = IntArray(3)
+        for(i in coins.indices){
+            intCoins[i] = coins[i].toInt()
+        }
     val ans :String = if(intCoins[2] <= intCoins[0] + intCoins[1]) "Yes" else "No"
-    println(ans)
+        println(ans)
 }
