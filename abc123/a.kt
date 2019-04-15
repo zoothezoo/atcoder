421547c56b5515335cbdeb980f802a87b3beb44f
zoothezoo <kohekoshien@gmail.com>
2019-04-15 14:42:24 +0900

    abc123

--- abc123/a.kt
+++ abc123/a.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>){
     val input :MutableList<Int> = mutableListOf()
     repeat(5){
-        input.add(readLine().toInt())
+        input.add(readLine()!!.toInt())
     }
-    val k = readLine().toInt
+    val k = readLine()!!.toInt()
 
     for(i in 0..4){
         for(j in 0..4){
